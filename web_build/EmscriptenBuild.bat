8cd16c52e33efbe91f00ffdc06598b1afdcfbc57
daredevil-grbll <crazyzacke@gmail.com>
2022-01-28 03:47:56 -0800

    Emscripten build script

---
+++ web_build/EmscriptenBuild.bat
@@ -0,0 +1,1 @@
+em++ -O3 -std=c++20 ..\game\src\main.cpp -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 -s USE_SDL_MIXER=2 -s ALLOW_MEMORY_GROWTH=1 -o game.html --use-preload-plugins --preload-file ./res
